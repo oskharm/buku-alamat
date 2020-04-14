717c7f17ca3ee0d536630e600e641929ddd50f9d
oskharm <oskha1800016087@webmail.uad.ac.id>
2020-04-14 21:01:40 +0700

    project buku-alamat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,71 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { TambahAlamatComponent } from './tambah-alamat/tambah-alamat.component';
+import { DetailAlamatComponent } from './detail-alamat/detail-alamat.component';
+import { DialogKonfirmasiComponent } from './dialog-konfirmasi/dialog-konfirmasi.component';
+ 
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Material design component
+import {MatToolbarModule} from '@angular/material/toolbar'; // material Toolbar
+import {MatButtonModule} from '@angular/material/button'; //  material Button
+
+// Daftar Card Alamat
+import {MatCardModule} from '@angular/material/card';  // material Card List
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu'; // material Menu
+
+// Dialog Penambahan Alamat
+ import {MatDialogModule} from '@angular/material/dialog';
+ import {MatFormFieldModule} from '@angular/material/form-field';
+ import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TambahAlamatComponent,
+    DetailAlamatComponent,
+    DialogKonfirmasiComponent,
   ],
+
+  entryComponents:[
+    TambahAlamatComponent,
+    DetailAlamatComponent,
+    DialogKonfirmasiComponent,
+  ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+
+    MatToolbarModule, // toolbar
+    MatButtonModule, // button
+
+    // Card List
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule, 
+
+    // Dialog Penambahan Alamat
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
---
+++ src/app/dialog-konfirmasi/dialog-konfirmasi.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-konfirmasi',
+  templateUrl: './dialog-konfirmasi.component.html',
+  styleUrls: ['./dialog-konfirmasi.component.css']
+})
+
+export class DialogKonfirmasiComponent implements OnInit {
+
+  constructor(
+  	public dialogRef:MatDialogRef<DialogKonfirmasiComponent>) { }
+
+  konfirmasi()
+  {
+    	this.dialogRef.close(true);
+  }
+ 
+
+  ngOnInit(): void {
+  }
+
+}
